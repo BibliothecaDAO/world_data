10f685d8ffb799027e87f94c0f40adc6534e321c
rvorias <raphaelvorias@gmail.com>
2022-02-26 12:03:51 +0100

    Add ga bag info and refactor make

--- README.md
+++ README.md
@@ -15,23 +15,42 @@ You can use QGIS to open and edit `realms_world.qgs`.
 |`crypts_sea.json` | Sea crypts |
 |`geo.json`| Defines order continents and islands |
 |`realms.json`| Defines where all realms are situated |
+|`ga_bags.json`| Data pulled from graphql (manually updated)|
 
 `datasources_geojson`
 
 |filename|info|
 |----------------:|:------------------|
-|`buffered_zones.geojson` | Buffer zone with an inland offset to make sure random samples don't land on the coastline |
-|`coast_2.geojson` | Buffer zone land outward of magnitude 2 |
+|`loot_bags.geojson` | Random loot bags scattered into the buffered zone |
 |`ga_bags.geojson` | Sampled randomly in each respective order |
-|`ocean.geojson` | Coarse ocean layer |
-|`coast_3.geojson` | Buffer zone land outward of magnitude 3 |
+|`crypts_land.geojson` | Land crypts |
+|`crypts_sea.geojson` | Sea crypts |
+|`buffered_zones.geojson` | Buffer zone with an inland offset to make sure random samples don't land on the coastline |
+|`sea_icons.geojson` | Unused
 |`order_highlights.geojson` | Layer to assist with order highlighting, not used
 |`buffered_zones_separate.geojson` | buffer zones with separated geometry |
-|`crypts_land.geojson` | Land crypts |
-|`loot_bags.geojson` | Random loot bags scattered into the buffered zone |
-|`sea_icons.geojson` | Unused
-|`crypts_sea.geojson` | Sea crypts |
 |`water_bodies.geojson` | Defines water bodies and backstories |
-|`coast_1.geojson` | Buffer zone land outward of magnitude 1 |
-|`fog.geojson` | Inland for of war layer |
 |`no_borders.geojson` | Merge of whole continents |
+|`fog.geojson` | Inland for of war layer |
+|`coast_1.geojson` | Buffer zone land outward of magnitude 1 |
+|`coast_2.geojson` | Buffer zone land outward of magnitude 2 |
+|`coast_3.geojson` | Buffer zone land outward of magnitude 3 |
+|`ocean.geojson` | Coarse ocean layer |
+
+## build
+
+In order to prepare all files, run: `$ python make.py`
+All necessary files will be put in the `build/` folder.
+
+For the ga bags, go to `https://thegraph.com/hosted-service/subgraph/treppers/genesisproject` and run:
+```
+{
+  adventurers {
+    id
+    order
+    currentOwner {
+      id
+    }
+  }
+}
+```
---
+++ make.py
@@ -0,0 +1,155 @@
+import os
+import shutil
+import json
+import random
+
+BUILD_DIR = "./build"
+
+# genesis project
+GA_BAGS_GRAPHQL = "./datasources_json/ga_bags.json"
+GA_BAGS_GEOJSON = "./datasources_geojson/ga_bags.geojson"
+
+# crypts and caverns
+CNC_METADATA = "./cnc-images/metadata/dungeons.json"
+LAND_CRYPTS_GEOJSON = "./datasources_geojson/crypts_land_order.geojson"
+SEA_CRYPTS_GEOJSON = "./datasources_geojson/crypts_sea.geojson"
+
+ORDERS = [ # random order here!
+    "Rage",
+    "Detection",
+    "Anger",
+    "Giants",
+    "Skill",
+    "Enlightenment",
+    "Power",
+    "Brilliance",
+    "Perfection",
+    "Titans",
+    "Fury",
+    "Reflection",
+    "the Twins",
+    "Vitriol",
+    "the Fox",
+    "Protection"
+]
+
+if os.path.isdir(BUILD_DIR):
+    shutil.rmtree(BUILD_DIR)
+os.mkdir(BUILD_DIR)
+
+def load_json(path):
+    with open(path, "r") as file:
+        data = json.load(file)
+    return data
+
+def save_json(data, path):
+    with open(f"{BUILD_DIR}/{path}", "w") as file:
+        json.dump(data, file, indent=2)
+
+def allocate_genesis_adventurers():
+    # load data sources
+    graphdata = load_json(GA_BAGS_GRAPHQL)
+    geodata = load_json(GA_BAGS_GEOJSON)
+    
+    # create fixed mapping
+    for ga in graphdata["data"]["adventurers"]:
+        for feature in geodata["features"]:
+            if ga["order"] == feature["properties"]["order_name"]:
+                if "ga_id" not in feature["properties"]:
+                    feature["properties"]["ga_id"] = ga["id"]
+                    break
+    new_data = [
+        {
+            "ga_id": feature["properties"]["ga_id"],
+            "order": feature["properties"]["order_name"],
+            "geometry": feature["geometry"]
+        }
+        for feature in geodata["features"] if "ga_id" in feature["properties"]
+    ]
+    save_json(new_data, "ga_bags.json")
+
+def allocate_crypts():
+    cnc_data = load_json(CNC_METADATA)
+    land_data = load_json(LAND_CRYPTS_GEOJSON)
+    sea_data = load_json(SEA_CRYPTS_GEOJSON)
+    random.seed(42)
+    random.shuffle(land_data["features"])
+
+    # create some useful lists
+    nones = []
+    environments = []
+    affinities = []
+
+    for cidx, crypt in enumerate(cnc_data["dungeons"]):
+        if crypt is None:
+            nones.append(cidx)
+            continue
+        env, aff = crypt["environment"], crypt["affinity"]
+        if env not in environments:
+            environments.append(env)
+        if aff not in affinities:
+            affinities.append(aff)
+
+    # sea crypts
+    sea_crypts = []
+
+    for idx, crypt in enumerate(cnc_data["dungeons"]):
+        if crypt is None:
+            continue
+        if crypt["environment"] == "Underwater Keep":
+            sea_crypts.append(crypt)
+            cnc_data["dungeons"][idx] = None
+            
+    for idx, co in enumerate(sea_data["features"]):
+        co["properties"].update(sea_crypts[idx])
+
+    # land crypts
+    ordered_crypts = {}
+    other_crypts = []
+    for idx, crypt in enumerate(cnc_data["dungeons"]):
+        if crypt is None:
+            continue
+        order = crypt["affinity"]
+        if order in ORDERS and order != "Underwater Keep":
+            if order not in ordered_crypts:
+                ordered_crypts[order] = [crypt]
+            else:
+                ordered_crypts[order].append(crypt)
+        else:
+            other_crypts.append(crypt)
+
+    # give priority to crypts with a tied order
+    for order, prio_crypts in ordered_crypts.items():
+        for prio_crypt in prio_crypts:
+            for cidx, feature in enumerate(land_data["features"]):
+                if feature["properties"]["order_name"] == order and len(feature["properties"]) == 2:
+                    land_data["features"][cidx]["properties"].update(prio_crypt)
+                    break
+
+    # allocate the residual crypts
+    for sec_crypt in other_crypts:
+        for cidx, feature in enumerate(land_data["features"]):
+            if len(feature["properties"]) == 2:
+                land_data["features"][cidx]["properties"].update(sec_crypt)
+                break
+
+    # combine everything
+    all_features = sea_data["features"] + land_data["features"]
+    all_features = [x for x in all_features if len(x["properties"]) > 2]
+
+    combined = {'type': 'FeatureCollection',
+        'name': 'crypts_sea',
+        'crs': {
+            'type': 'name',
+            'properties': {'name': 'urn:ogc:def:crs:OGC:1.3:CRS84'}
+        },
+        'features': all_features
+    }
+
+    save_json(sea_data, "crypts_sea.json")
+    save_json(land_data, "crypts_land.json")
+    save_json(combined, "crypts_all.json")
+
+
+allocate_genesis_adventurers()
+allocate_crypts()
